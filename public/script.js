32ca5caf3052253e1d0723d52dd21ae4b7be6d7d
Siddharth <siddharth6780@gmail.com>
2021-07-01 23:36:02 +0530

    Changes

--- public/script.js
+++ public/script.js
@@ -1,11 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 let myVideoStream;
-const myPeer = new Peer(undefined, {
-    path: '/peerjs',
-    host: '/',
-    port: '443'
-})
+let peer = new Peer({ host: 'peerjs-server.herokuapp.com', secure: true, port: 443 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
