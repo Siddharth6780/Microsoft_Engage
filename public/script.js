2f3509891756e7d71534d9b6a57f313f8aee0944
Siddharth <siddharth6780@gmail.com>
2021-07-05 21:00:17 +0530

    Many Changes

--- public/script.js
+++ public/script.js
@@ -2,9 +2,9 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
-    // path: '/peerjs',
+    path: '/peerjs',
     host: '/',
-    port: '3001'
+    port: '443'
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
